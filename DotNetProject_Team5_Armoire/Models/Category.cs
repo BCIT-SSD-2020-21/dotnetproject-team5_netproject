f44a3f1b71c198aea36cc19ad27b5e74e9def166
KulveerBrar <brarkulveer15@gmail.com>
2020-12-08 09:59:04 -0800

    Properties added to Category Model #12

--- DotNetProject_Team5_Armoire/Models/Category.cs
+++ DotNetProject_Team5_Armoire/Models/Category.cs
@@ -7,6 +7,13 @@ namespace DotNetProject_Team5_Armoire.Models
 {
     public class Category
     {
-        
+        public virtual Guid Id { get; protected set; }
+        public string Name { get; private set; }
+
+        public Category(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
