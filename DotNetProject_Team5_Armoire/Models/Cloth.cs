20fe49d332cc71bc174081e2e55f47a03c8f3ff0
Deepika <deepikaverma087@gmail.com>
2020-12-08 11:27:11 -0800

    Fixed Cloth model And seeder class

--- DotNetProject_Team5_Armoire/Models/Cloth.cs
+++ DotNetProject_Team5_Armoire/Models/Cloth.cs
@@ -7,17 +7,19 @@ namespace DotNetProject_Team5_Armoire.Models
 {
     public class Cloth : BaseEntity
     {
-        public Guid UserId { get; set; }
+        public int UserId { get; set; }
         public string ClothName { get; set; }
         public bool IsClean { get; set; }
         public string PictureUri { get; set; }
+        public int CategoryId { get; set; }
 
-        public Cloth (Guid userid, String clothName, bool isClean, String pictureUri)
+        public Cloth (int userid, String clothName, bool isClean, String pictureUri, int categoryId)
         {
             UserId = userid;
             ClothName = clothName;
             IsClean = isClean;
             PictureUri = pictureUri;
+            CategoryId = categoryId;
         }
     }
 }
