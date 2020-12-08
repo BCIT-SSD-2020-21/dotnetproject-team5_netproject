9ed3132d6fdcf38dd947790fa9f6c391d410a106
gabay@my.bcit.ca <gabay@my.bcit.ca>
2020-12-08 11:40:43 -0800

    ClothDbContext configuration fix

--- DotNetProject_Team5_Armoire/Startup.cs
+++ DotNetProject_Team5_Armoire/Startup.cs
@@ -27,7 +27,7 @@ namespace DotNetProject_Team5_Armoire
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<ClothDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
