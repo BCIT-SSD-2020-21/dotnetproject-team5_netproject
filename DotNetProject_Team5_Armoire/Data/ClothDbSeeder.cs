a954350238390c2ced9ecd2515c36b579f3e01b2
gabay@my.bcit.ca <gabay@my.bcit.ca>
2020-12-08 12:36:37 -0800

    Seeder class fix

--- DotNetProject_Team5_Armoire/Data/ClothDbSeeder.cs
+++ DotNetProject_Team5_Armoire/Data/ClothDbSeeder.cs
@@ -1,35 +1,35 @@
-﻿//using DotNetProject_Team5_Armoire.Models;
-//using Microsoft.EntityFrameworkCore;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
-//using System.Linq;
+﻿using DotNetProject_Team5_Armoire.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
-//namespace DotNetProject_Team5_Armoire.Data
-//{
-//    public class ClothDbSeeder
-//    {
-//        public static async Task SeedAsync(ClothDbContext clothcontext)
-//        {
-//            if (!await clothcontext.Clothes.AnyAsync())
-//            {
-//                await clothcontext.Clothes.AddRangeAsync(
-//                    GetPreconfiguredItems());
+namespace DotNetProject_Team5_Armoire.Data
+{
+    public class ClothDbSeeder
+    {
+        public static async Task SeedAsync(ClothDbContext clothcontext)
+        {
+            if (!await clothcontext.Clothes.AnyAsync())
+            {
+                await clothcontext.Clothes.AddRangeAsync(
+                    GetPreconfiguredItems());
 
-//                await clothcontext.SaveChangesAsync();
-//            }
+                await clothcontext.SaveChangesAsync();
+            }
 
-//            static IEnumerable<Cloth> GetPreconfiguredItems()
-//            {
-//                return new List<Cloth>()
-//                {   //in sequence of userid, name, isclean, picture, categoryid
-//                    new Cloth(1,"Crop Top",false,"/images/tops/croptop.jpg",1),
-//                    new Cloth(1,"T-shirt",true,"/images/tops/tshirt.jpg",1),
-//                    new Cloth(2,"Shirt",true,"/images/tops/shirt.jpg",1),
-//                    new Cloth(2,"Straight Pants",false,"/images/bottoms/pant.jpg",2),
-//                    new Cloth(2,"Jeans",true,"/images/bottoms/jeans.jpg",2),
-//                    new Cloth(1,"Sweatpants",false,"/images/bottoms/sweatpants.jpg",2),
-//                };
-//            }
-//        }
-//    }
-//}
+            static IEnumerable<Clothing> GetPreconfiguredItems()
+            {
+                return new List<Clothing>()
+                {   //in sequence of userid, name, isclean, picture, categoryid
+                    new Clothing("1","Crop Top",false,"/images/tops/croptop.jpg",1),
+                    new Clothing("1","T-shirt",true,"/images/tops/tshirt.jpg",1),
+                    new Clothing("2","Shirt",true,"/images/tops/shirt.jpg",1),
+                    new Clothing("2","Straight Pants",false,"/images/bottoms/pant.jpg",2),
+                    new Clothing("2","Jeans",true,"/images/bottoms/jeans.jpg",2),
+                    new Clothing("1","Sweatpants",false,"/images/bottoms/sweatpants.jpg",2),
+                };
+            }
+        }
+    }
+}
