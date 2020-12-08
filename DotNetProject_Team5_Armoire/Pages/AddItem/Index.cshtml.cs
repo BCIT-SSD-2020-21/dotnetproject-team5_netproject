0838b60639b3922a6f6c868592964cb42aab461e
gabay@my.bcit.ca <gabay@my.bcit.ca>
2020-12-08 15:24:25 -0800

    Form update

--- DotNetProject_Team5_Armoire/Pages/AddItem/Index.cshtml.cs
+++ DotNetProject_Team5_Armoire/Pages/AddItem/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetProject_Team5_Armoire.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,13 +10,25 @@ namespace DotNetProject_Team5_Armoire.Pages.AddItem
 {
     public class IndexModel : PageModel
     {
+        // connect to database
+        protected readonly ClothDbContext db;
+
+        public IndexModel(ClothDbContext db)
+        {
+            this.db = db;
+        }
+
         public void OnGet()
         {
+            
         }
 
         public IActionResult OnPost()
         {
             // When the post request is made we want to create a new Cloth
+
+
+
             // SaveChanges to Database
             return RedirectToPage();
         }
