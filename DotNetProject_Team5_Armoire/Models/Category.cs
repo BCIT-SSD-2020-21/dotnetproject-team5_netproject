3719b8c585c1ac354150bb318d99ebad7cf292b5
KulveerBrar <brarkulveer15@gmail.com>
2020-12-08 10:02:54 -0800

    Category Model Updated #12

--- DotNetProject_Team5_Armoire/Models/Category.cs
+++ DotNetProject_Team5_Armoire/Models/Category.cs
@@ -5,14 +5,12 @@ using System.Threading.Tasks;
 
 namespace DotNetProject_Team5_Armoire.Models
 {
-    public class Category
+    public class Category:BaseEntity
     {
-        public virtual Guid Id { get; protected set; }
         public string Name { get; private set; }
 
         public Category(Guid id, string name)
         {
-            Id = id;
             Name = name;
         }
     }
