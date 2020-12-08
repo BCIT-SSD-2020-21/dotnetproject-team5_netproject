00c956417c8f54c5c827338712f8a2447665e806
gabay@my.bcit.ca <gabay@my.bcit.ca>
2020-12-08 10:00:47 -0800

    #17 Add ClothDbContext

---
+++ DotNetProject_Team5_Armoire/Data/ClothDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetProject_Team5_Armoire.Data
+{
+    public class ClothDbContext : DbContext
+    { 
+
+        public ClothDbContext(DbContextOptions<ClothDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cloth> Clothes { get; set; }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
