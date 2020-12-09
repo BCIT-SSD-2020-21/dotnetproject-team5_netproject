cc855f83555dee32c9c7fc0aecb2d03a8fdc29c4
gabay@my.bcit.ca <gabay@my.bcit.ca>
2020-12-08 17:21:10 -0800

    #14 Create new Clothing functionality implemented

--- DotNetProject_Team5_Armoire/Pages/AddItem/Index.cshtml.cs
+++ DotNetProject_Team5_Armoire/Pages/AddItem/Index.cshtml.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using DotNetProject_Team5_Armoire.Data;
+using DotNetProject_Team5_Armoire.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -18,15 +20,36 @@ namespace DotNetProject_Team5_Armoire.Pages.AddItem
             this.db = db;
         }
 
+        public Clothing clothing { get; set; }
+
         public void OnGet()
         {
             
         }
 
-        public IActionResult OnPost()
+        public IActionResult OnPost(string clothingName, string category, bool isClean = false)
         {
             // When the post request is made we want to create a new Cloth
 
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            int categoryId;
+            
+            switch (category)
+            {     
+                case "Top":
+                    categoryId = 1;
+                    break;
+                case "Bottom":
+                    categoryId = 2;
+                    break;
+                default:
+                    categoryId = 1;
+                    break;
+            }
+
+            Clothing clothing = new Clothing(userId, clothingName, isClean, "newImages", categoryId);
+            db.Clothes.Add(clothing);
+            db.SaveChanges();
 
 
             // SaveChanges to Database
