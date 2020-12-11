86d2a6ebf5b7d827731fb5c283718a5bc51a60a6
Deepika Verma <deepikaverma087@gmail.com>
2020-12-11 15:54:42 -0800

    Added category filter list #80

--- DotNetProject_Team5_Armoire/Pages/Dashboard.cshtml.cs
+++ DotNetProject_Team5_Armoire/Pages/Dashboard.cshtml.cs
@@ -17,6 +17,10 @@ namespace DotNetProject_Team5_Armoire.Pages
         //Access database
         protected readonly ClothDbContext db;
         public IQueryable<Clothing> Clothes { get; set; }
+        public IQueryable<Clothing> Tops { get; set; }
+        public IQueryable<Clothing> Bottoms { get; set; }
+        public IQueryable<Category> Categories { get; set; }
+
         public DashboardModel(ClothDbContext db)
         {
             this.db = db;
@@ -25,14 +29,17 @@ namespace DotNetProject_Team5_Armoire.Pages
         public void OnGet()
         {
             string userId;
+            
 
             if (User.Identity.IsAuthenticated)
             {
                 userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
                 Clothes = db.Clothes
                     .Where(c => c.OwnerId == userId);
-                    
+                
             }
+
+            Categories = db.Categories.Where(c => c.Id == 1 || c.Id == 2);
         }
     }
 }
