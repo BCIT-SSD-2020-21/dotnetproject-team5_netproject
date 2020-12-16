1fb0c688c63ba7206bd14ce1de4c52e7b2437b36
gabay@my.bcit.ca <gabay@my.bcit.ca>
2020-12-16 11:35:12 -0800

    #119 Delete method added to AWS Service, Delete service added to Delete Item Page

--- DotNetProject_Team5_Armoire/Pages/DeleteItem.cshtml.cs
+++ DotNetProject_Team5_Armoire/Pages/DeleteItem.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using a00893112s3clothesimages_S3_bucket.Services;
 using DotNetProject_Team5_Armoire.Data;
 using DotNetProject_Team5_Armoire.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,14 +15,17 @@ namespace DotNetProject_Team5_Armoire.Pages
     public class DeleteItemModel : PageModel
     {
         private readonly ClothDbContext _db;
+        private readonly IAWSS3Service _awsS3Service;
+
         public List<Clothing> Clothes = new List<Clothing>();
         public List<Clothing> isDirty = new List<Clothing>();
         public string msg = "";
         public string popoverclass = "";
 
-        public DeleteItemModel(ClothDbContext db)
+        public DeleteItemModel(ClothDbContext db, IAWSS3Service awsS3Service)
         {
             _db = db;
+            _awsS3Service = awsS3Service;
         }
 
         public Clothing Cloth { get; set; }
@@ -83,8 +87,19 @@ namespace DotNetProject_Team5_Armoire.Pages
 
             if (Cloth != null)
             {
+                // Remove image from AWS
+                try
+                {
+                    _awsS3Service.DeleteFile(Cloth.PictureUri);
+                }
+                catch (Exception e)
+                {
+                    ModelState.AddModelError("S3 Delete", e.Message);
+                }
                 _db.Clothes.Remove(Cloth);
                 await _db.SaveChangesAsync();
+
+
             }
 
             return RedirectToPage("./Dashboard");
